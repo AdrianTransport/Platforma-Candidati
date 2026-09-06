8310a29b5cfa12a8cadb89e97af76866f04eec00
PlatformaPolitica <sanierungauto@gmail.com>
2026-09-07 02:03:54 +0300

    Repara initializarea Netlify Blobs in functia Lambda

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,12 @@
 import serverless from 'serverless-http';
+import { connectLambda } from '@netlify/blobs';
 import { createApp } from '../../app.js';
 
 let handlerPromise;
 
 export const handler = async (event, context) => {
+  connectLambda(event);
+
   if (!handlerPromise) {
     handlerPromise = createApp().then((app) => serverless(app));
   }
